39685f74cb68aaea483939581384310fb05bfd17
Marek Kubica <marek@tarides.com>
2024-02-29 11:44:19 +0100

    Decode QR payload and dump on stdout

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,16 @@
-let () = print_endline "Hello, World!"
+open Base
+
+let main qr_value =
+  match String.chop_prefix ~prefix:"nux://" qr_value with
+  | None -> failwith "no nux:// prefix"
+  | Some payload -> (
+      match String.chop_prefix ~prefix:"MightyAmp:" payload with
+      | None -> failwith "Not MightyAmp compatible payload"
+      | Some b64 -> (
+          match Base64.decode b64 with
+          | Error (`Msg msg) -> failwith msg
+          | Ok nux_data -> Stdio.print_endline nux_data))
+
+let () =
+  let qr_value = (Sys.get_argv ()).(1) in
+  main qr_value
