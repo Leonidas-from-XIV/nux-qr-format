9c19c04c15364c50af6b4df2fc81102dd85625ca
Marek Kubica <marek@tarides.com>
2024-03-21 15:09:59 +0100

    Print newline if dumping to terminal

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,13 @@
 open Base
 
+let dump_data nux_data =
+  let oc = Stdio.stdout in
+  let oc_descr = Unix.descr_of_out_channel oc in
+  Stdio.Out_channel.output_string oc nux_data;
+  match Unix.isatty oc_descr with
+  | true -> Stdio.Out_channel.newline oc
+  | false -> ()
+
 let main qr_value =
   match String.chop_prefix ~prefix:"nux://" qr_value with
   | None -> failwith "no nux:// prefix"
@@ -9,7 +17,7 @@ let main qr_value =
       | Some b64 -> (
           match Base64.decode b64 with
           | Error (`Msg msg) -> failwith msg
-          | Ok nux_data -> Stdio.print_string nux_data))
+          | Ok nux_data -> dump_data nux_data))
 
 let () =
   let qr_value = (Sys.get_argv ()).(1) in
